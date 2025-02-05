ca411f37586a3d90f8a011dbb36e424ad5437702
Hyemin <star970909@gmail.com>
2025-02-05 16:32:44 +0900

    2025-02-05 sql문 컬럼명 수정
    code -> gCode
    category -> gCategory
    name -> gName
    ...

--- datas/db1_refrigerator.sql
+++ datas/db1_refrigerator.sql
@@ -1,26 +1,26 @@
 use team5;
 
 create table refrigerator (
-	code varchar(20),
-	category varchar(20) not null,
-	name varchar(20) not null,
-	stock int default(1) not null,
-	primary key (code)
+	gCode varchar(20),
+	gCategory varchar(20) not null,
+	gName varchar(20) not null,
+	rStock int default(1) not null,
+	primary key (gCode)
 );
 
-insert into refrigerator (code, category, name, stock) values ("SI-0001", "반찬", "김치", 1);
-insert into refrigerator (code, category, name, stock) values ("SI-0002", "반찬", "찜닭", 1);
-insert into refrigerator (code, category, name, stock) values ("SC-0001", "양념", "굴소스", 1);
-insert into refrigerator (code, category, name, stock) values ("SC-0002", "양념", "다진마늘", 1);
-insert into refrigerator (code, category, name, stock) values ("VG-0001", "채소", "양배추", 1);
-insert into refrigerator (code, category, name, stock) values ("FR-0001", "과일", "귤", 10);
-insert into refrigerator (code, category, name, stock) values ("MT-0001", "육류", "닭가슴살", 4);
-insert into refrigerator (code, category, name, stock) values ("MT-0002", "육류", "돼지고기 앞다리살", 2);
-insert into refrigerator (code, category, name, stock) values ("FS-0001", "생선", "고등어", 1);
-insert into refrigerator (code, category, name, stock) values ("DA-0001", "유제품", "슬라이스 치즈", 20);
-insert into refrigerator (code, category, name, stock) values ("DA-0002", "유제품", "달걀", 5);
-insert into refrigerator (code, category, name, stock) values ("SN-0001", "간식", "크림빵", 1);
-insert into refrigerator (code, category, name, stock) values ("FZ-0001", "냉동식품", "군만두", 2);
-insert into refrigerator (code, category, name, stock) values ("FZ-0002", "냉동식품", "감자튀김", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("SI-0001", "반찬", "김치", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("SI-0002", "반찬", "찜닭", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("SC-0001", "양념", "굴소스", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("SC-0002", "양념", "다진마늘", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("VG-0001", "채소", "양배추", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("FR-0001", "과일", "귤", 10);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("MT-0001", "육류", "닭가슴살", 4);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("MT-0002", "육류", "돼지고기 앞다리살", 2);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("FS-0001", "생선", "고등어", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("DA-0001", "유제품", "슬라이스 치즈", 20);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("DA-0002", "유제품", "달걀", 5);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("SN-0001", "간식", "크림빵", 1);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("FZ-0001", "냉동식품", "군만두", 2);
+insert into refrigerator (gCode, gCategory, gName, rStock) values ("FZ-0002", "냉동식품", "감자튀김", 1);
 
 commit;
--- datas/db4_cart.sql
+++ datas/db4_cart.sql
@@ -3,12 +3,12 @@ use team5;
 CREATE TABLE cart (
   num int NOT NULL AUTO_INCREMENT,
   userid varchar(20) DEFAULT NULL,
-  code varchar(20) NOT NULL,
+  gCode varchar(20) NOT NULL,
   amount int NOT NULL,
   cartDate date DEFAULT (curdate()),
   PRIMARY KEY (num),
   KEY cart_userid_fk (userid),
-  KEY cart_code_fk (code),
-  CONSTRAINT cart_code_fk FOREIGN KEY (code) REFERENCES goods (code) ON DELETE CASCADE,
+  KEY cart_gCode_fk (gCode),
+  CONSTRAINT cart_gCode_fk FOREIGN KEY (gCode) REFERENCES goods (gCode) ON DELETE CASCADE,
   CONSTRAINT cart_userid_fk FOREIGN KEY (userid) REFERENCES member (userid) ON DELETE CASCADE
 );
